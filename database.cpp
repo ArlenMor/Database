976eb89dc961ed0615c664bfe5450ab908802916
Demonik4052 <Demonik4053@gmail.com>
2020-07-06 16:11:26 +0700

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : database.cpp
 // Author      : ArlenMor
-// Version     : 0.2
+// Version     : 1.0 release
 // Description : Database. The database stores pairs of the form: date - event
 // Description : We define the date as a string of the form Year-Month-Day, where Year, Month and Day are integers.
 // Operation   : 1. Add event								: 	Add date event
@@ -12,9 +12,12 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include <vector>
 #include <map>
+#include <set>
+#include <stdexcept>
+#include <sstream>
 using namespace std;
 
 class Date{
@@ -27,9 +30,12 @@ public:
 	}
 	Date(int new_year, int new_month, int new_day)
 	{
-		//check date
 		year = new_year;
+		if(new_month > 12 || new_month < 1)
+			throw logic_error("Month value is invalid: " + to_string(new_month));
 		month = new_month;
+		if(new_day > 31 || new_day < 1)
+			throw logic_error("Day value is invalid: " + to_string(new_day));
 		day = new_day;
 	}
 	int get_year() const;
@@ -48,15 +54,11 @@ int Date::get_year() const
 
 int Date::get_month() const
 {
-	if(month <= 0 && month > 12)
-		return -1; //handle the error
 	return month;
 }
 
 int Date::get_day() const
 {
-	if(day <= 0 && day > 31)
-		return -1; //handle the error
 	return day;
 }
 
@@ -72,16 +74,11 @@ bool operator < (const Date& lhs, const Date& rhs)
 	return lhs.get_year() < rhs.get_year();
 }
 
-bool operator == (const Date& lhs, const Date& rhs)
-{
-	return (lhs.get_year() == rhs.get_year() &&
-			lhs.get_month() == rhs.get_month() &&
-			lhs.get_day() == rhs.get_day());
-}
-
 ostream& operator << (ostream& stream, const Date& date)
 {
-	return stream << date.get_year() << "-" << date.get_month() << "-" << date.get_day();
+	return stream << setw(4) << setfill('0') << date.get_year() <<
+			"-" << setw(2) << setfill('0') << date.get_month() <<
+			"-" << setw(2) << setfill('0') << date.get_day();
 }
 
 class Database {
@@ -89,79 +86,141 @@ public:
 	void add_event (const Date& date, const string& event);
 	bool delete_event (const Date& date, const string& event);
 	int delete_date (const Date& date);
-	vector<string> Find (const Date& date) const;
+	set<string> find (const Date& date) const;
 
-	void print_database();
+	void print() const;
 private:
-	map<Date, vector<string>> date_events;
+	map<Date, set<string>> storage;
 };
 
 void Database::add_event(const Date& date, const string& event)
 {
-	date_events[date].push_back(event);
+	storage[date].insert(event);
 }
 
 bool Database::delete_event(const Date& date, const string& event)
 {
-	//rewrite, rework!!!!!!
-	for(auto it_map = date_events.begin(); it_map != date_events.end(); ++it_map)
+	if(storage.count(date) > 0 && storage[date].count(event) > 0)
 	{
-		if((*it_map).first == date)
-		{
-			for(auto it_v = (*it_map).second.begin(); it_v != (*it_map).second.end(); ++it_v)
-			{
-				if((*it_v) == event)
-				{
-					(*it_map).second.erase(it_v);
-					return true;
-				}
-			}
-		}
+		storage[date].erase(event);
+		return true;
 	}
 	return false;
 }
 
 int Database::delete_date(const Date& date)
 {
-	//add check
-	int count_event = date_events[date].size();
-	date_events.erase(date);
-	return count_event;
+	if(storage.count(date) == 0)
+		return 0;
+	else{
+		int count_event = storage[date].size();
+		storage.erase(date);
+		return count_event;
+	}
 }
 
-void Database::print_database()
+set<string> Database::find(const Date& date) const
 {
-	for(const auto& item : date_events)
+	if(storage.count(date) > 0)
+		return storage.at(date);
+	else
+		return {};
+}
+
+void Database::print() const
+{
+	for(const auto& item : storage)
 	{
-		cout << "Date: " << item.first << " Events: ";
 		for(const auto& event : item.second)
 		{
-			cout << event << " ";
+			cout << item.first << " " << event << endl;
 		}
-		cout << endl;
 	}
+}
+
+Date parse_date(const string& date)
+{
+	stringstream date_stream(date);
+	int year;
+	date_stream >> year;
+	if(date_stream.peek() != '-')
+		throw logic_error ("Wrong date format: " + date);
+	date_stream.ignore();
+
+	if(date_stream.peek() == EOF)
+		throw logic_error("Wrong date format: " + date);
+
+	int month;
+	date_stream >> month;
+	if(date_stream.peek() != '-')
+		throw logic_error("Wrong date format: " + date);
+	date_stream.ignore();
+
+	if(date_stream.peek() == EOF)
+		throw logic_error("Wrong date format: " + date);
+
+	int day;
+	date_stream >> day;
+	if(!date_stream.eof())
+		throw logic_error("Wrong date format: " + date);
+
+	return Date(year, month, day);
 }
 
 int main() {
-	Database database;
-	string command;
-	while (getline(cin, command)) {
-		if(command == "Add")
-		{
-			cout << "I here" << endl;
-			int year, month, day;
-			cin >> year; cin.ignore(1);
-			cin >> month; cin.ignore(1);
-			cin >> day;
-			string event;
-			cin >> event;
-			Date date = {year, month, day};
-			database.add_event(date, event);
-			database.print_database();
-		}else if (command == "Del")
-		{
-
+	try{
+		Database database;
+		string command_line;
+		while (getline(cin, command_line)) {
+			stringstream ss(command_line);
+			string command;
+			ss >> command;
+			if(command == "Add")
+			{
+				string date_str, event;
+				ss >> date_str >> event;
+				Date date = parse_date(date_str);
+				database.add_event(date, event);
+			}else if(command == "Del")
+			{
+				string date_str;
+				ss >> date_str;
+				string event;
+				if(!ss.eof())
+					ss >> event;
+				Date date = parse_date(date_str);
+				if(event.empty())
+				{
+					const int count = database.delete_date(date);
+					cout << "Deleted " << count << " events" << endl;
+				}else{
+					if(database.delete_event(date, event))
+					{
+						cout << "Deleted successfully" << endl;
+					}else{
+						cout << "Event not found" << endl;
+					}
+				}
+			}else if(command == "Find")
+			{
+				string date_str;
+				ss >> date_str;
+				Date date = parse_date(date_str);
+				for(const string& event : database.find(date))
+				{
+					cout << event << endl;
+				}
+			}else if (command == "Print")
+			{
+				database.print();
+			}else if (command.empty())
+				continue;
+			else
+				throw logic_error("Unknown command: " + command);
 		}
+	}catch (exception& ex){
+		cout << ex.what() << endl;
 	}
+
 	return 0;
 }
